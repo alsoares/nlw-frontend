3c7f05fed712f7800340f8842510c7192d8a835a
alsoares <andresoaresti@gmail.com>
2020-06-03 13:05:17 -0300

    state

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 
 import Header from './Header';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  function handleButtonClick(){
+    setCounter(counter + 1);
+  }
   return (
     <div>
       <Header title="Hello World" />
 
-      <h1>Conteudo da aplicação</h1>
+      <h1>{counter}</h1>
+      <button type="button" onClick={handleButtonClick}>Aumentar</button>
     </div> 
   );
 }
